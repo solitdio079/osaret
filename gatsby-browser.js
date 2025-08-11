d24baa36d74bfd212e0c4cb8e041afe9e057df07
solitdio079 <solitdio079@gmail.com>
2025-08-11 22:04:52 +0300

    Add splash screen

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,4 +1,11 @@
 import './src/styles/global.css';
+import React from "react"
+import RootWrapper from "./src/components/RootWrapper"
+
+export const wrapRootElement = ({ element }) => {
+  return <RootWrapper>{element}</RootWrapper>
+}
+
 
 export const onClientEntry = async () => {
     if (typeof window !== 'undefined') {
