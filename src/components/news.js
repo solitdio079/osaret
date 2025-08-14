d6f2b91147091a989279c1b89f6ce77b693ec983
solitdio079 <solitdio079@gmail.com>
2025-08-14 14:21:38 +0300

    Add activities

--- src/components/news.js
+++ src/components/news.js
@@ -60,6 +60,30 @@ const News = () => {
             </p>
           </div>
         </div>
+        <div class="card image-full sm:max-w-sm  intersect:motion-preset-slide-left intersect:motion-delay-[900ms] intersect:motion-ease-spring-bouncier">
+          <figure>
+            <img src="https://cdn.flyonui.com/fy-assets/components/card/image-5.png" alt="overlay image" />
+          </figure>
+          <div class="card-body">
+            <h2 class="card-title mb-2.5 text-white">Activity 5</h2>
+            <p class="text-white">
+              Boost your brand's visibility and engagement through targeted
+              marketing strategies.
+            </p>
+          </div>
+        </div>
+        <div class="card image-full sm:max-w-sm  intersect:motion-preset-slide-left intersect:motion-delay-[900ms] intersect:motion-ease-spring-bouncier">
+          <figure>
+            <img src="https://cdn.flyonui.com/fy-assets/components/card/image-5.png" alt="overlay image" />
+          </figure>
+          <div class="card-body">
+            <h2 class="card-title mb-2.5 text-white">Activity 6</h2>
+            <p class="text-white">
+              Boost your brand's visibility and engagement through targeted
+              marketing strategies.
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
