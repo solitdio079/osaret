a5482d3dd73b29bf4db3b2ceeeeda61f23b71b47
solitdio079 <solitdio079@gmail.com>
2025-08-14 20:51:08 +0300

    Add new aproach

--- src/components/values.js
+++ src/components/values.js
@@ -92,7 +92,7 @@ const Values = () => {
                   <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
                 </svg>
                 <span class="text-base font-normal leading-tight   ms-3">
-                locale et inclusive
+                Approche locale et inclusive
 
                 </span>
               </li>
