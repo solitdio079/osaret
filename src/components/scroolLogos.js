de1fe69e5fbaf1094e15da7ea98f2dffd7bd6778
solitdio079 <solitdio079@gmail.com>
2025-08-14 15:39:11 +0300

    Add new scroll text

--- src/components/scroolLogos.js
+++ src/components/scroolLogos.js
@@ -8,10 +8,17 @@ const services = [
   partner1,partner2, partner3, partner4,partner5
 ]
 
+const infos = [
+  "La France reconnaît officiellement son rôle dans la répression violente des mouvements indépendantistes camerounais (1945–1971).",
+  "Redémarrage d’un réacteur à la centrale nucléaire de Gravelines après l’arrêt causé par une invasion de méduses.",
+  "Un dernier vendeur de journaux à la criée à Paris honoré par la Légion d’honneur.",
+  "Point sur la situation en France : l’alerte canicule est levée, mais les fortes températures persistent."
+]
+
 
 export default function ScrollLogos() {
-    return (
-      <div className="flex gap-6 overflow-hidden duration-300 [mask-image:_linear-gradient(to_right,transparent_0,_black_50px,_black_calc(100%-50px),transparent_100%)]">
+    return (<>
+     <div className="flex gap-6 overflow-hidden duration-300 [mask-image:_linear-gradient(to_right,transparent_0,_black_50px,_black_calc(100%-50px),transparent_100%)]">
         <div className="flex flex-nowrap gap-6 animate-logo mx-3">
              
             {services.map(service => 
@@ -38,6 +45,25 @@ export default function ScrollLogos() {
      
        
       </div>
+      <div className="flex gap-6 overflow-hidden duration-300 [mask-image:_linear-gradient(to_right,transparent_0,_black_50px,_black_calc(100%-50px),transparent_100%)]">
+        <div className="flex flex-nowrap gap-6 animate-news mx-3">
+             
+            {infos.map(info => 
+                  <p className="text-white "> {info} </p>
+            )}
+           
+    
+         
+        </div>
+       
+       
+       
+     
+     
+       
+      </div>
+    </>
+     
     );
   }
   
