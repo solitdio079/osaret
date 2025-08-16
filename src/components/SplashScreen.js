d8bba4f765ece987013015c5fb954ec22fa3f4ca
solitdio079 <solitdio079@gmail.com>
2025-08-17 00:49:06 +0300

    Add new contact

--- src/components/SplashScreen.js
+++ src/components/SplashScreen.js
@@ -9,7 +9,7 @@ const SplashScreen = () => {
     initial={{ opacity: 1 }}
     animate={{ opacity: 0 }}
     transition={{ delay: 1.5, duration: 0.5 }} data-theme="perplexity" className="flex w-full flex-col h-screen gap-3 justify-center items-center">
-    <img src={logo} className="w-48" alt="" />
+    <img src={logo} className="w-48 lg:w-96" alt="" />
     <span className="loading loading-ball"></span>
 
 </motion.div>
